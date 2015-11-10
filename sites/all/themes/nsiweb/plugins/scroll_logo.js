88df6baa1c57101d3fbd66d5ba5717c625f59318
Joshua Fabara <joshuafabara@gmail.com>
2015-11-10 10:28:46 -0500

    - favicons and theme jquery settings

--- sites/all/themes/nsiweb/plugins/scroll_logo.js
+++ sites/all/themes/nsiweb/plugins/scroll_logo.js
@@ -2,8 +2,6 @@
   Drupal.behaviors.ScrollLogo = {
     attach: function (context, settings) {
       $( window ).scroll(function() {
-        // $( "span" ).css( "display", "inline" ).fadeOut( "slow" );
-        console.log('hay scroll'+$( window ).scrollTop());
         if($( window ).scrollTop() > 0)
         {
           $('a.logo.navbar-btn').addClass('scrolled-logo');
